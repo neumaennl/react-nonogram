f2bb7a35885ee0a71699a4c34a2036c0be0ddca6
neumaennl <neumaennl@gmx.net>
2022-03-30 08:45:46 +0200

    initial commit with example from tutorial at handsonreact.com

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
+
+function App() {
+  return (
+    <div className="container">
+      <ProjectsPage />
+   </div>
+  );
+}
+
+export default App;
