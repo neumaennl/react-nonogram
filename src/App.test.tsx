799cc6a39e6d70f6edbfd0c20d1c430d6957c980
neumaennl <neumaennl@gmx.net>
2022-10-27 14:39:23 +0200

    removed superfluous test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders nonogram', () => {
-  const { container } = render(<App />);
-  // eslint-disable-next-line testing-library/no-node-access
-  expect(container.firstElementChild).toHaveClass("nonogram");
-});
