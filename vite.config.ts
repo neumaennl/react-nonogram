8b7a7d0ee9d262bfa4ab4c7a372faa3bf96b9f51
Neumaennl <neumaennl@gmx.net>
2026-03-31 08:08:08 +0200

    fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgrPlugin()],
+  plugins: [react(), svgr()],
   base: '',
 });
